5751228d3df545f647153215e57f92769233c35d
Fahim Chowdhury <fahim@gmx.com>
2014-03-27 10:39:24 +0000

    added html code in code block issue

--- dev/lib/markdownHTML.js
+++ dev/lib/markdownHTML.js
@@ -14,12 +14,39 @@ var MarkdownHTML = (function() {
       }
     },
     convertElement : function(element) {
+      var _this = this;
       marked(element.innerHTML, function(err, content) {
         if (err)
           throw err;
+
+        //convet special chars
+        if (content)
+          content = _this.convertCodeToText(content);
+
         element.innerHTML = content;
         hljs.initHighlightingOnLoad();
       });
+
+    },
+    convertCodeToText : function(content) {
+      content = content.replace(/\r?\n|\r/g, "<br>");
+
+      var pattern = /<code[^>]*>((.|[\n\r])*)<\/code>/img;
+      var codeTags = content.match(pattern);
+
+      for (var a = 0; a < codeTags.length; a++) {
+        var code = codeTags[a];
+        code = code.replace("<code>", "");
+        code = code.replace("</code>", "");
+
+        var escapedCode = escape(code);
+
+        var newCode = "<code>" + escapedCode + "</code>";
+
+        content = content.replace(code, newCode);
+
+      }
+      return content;
 
     },
     convertToHTML : function(mContent) {
